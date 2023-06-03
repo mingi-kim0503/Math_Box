712ca4fca975c6bb70c42faaee7c869b4794a293
MingiKim <danielkim.mingi@gmail.com>
2023-06-03 03:07:22 +0000

    sdfoijd

--- calculator.py
+++ calculator.py
@@ -10,8 +10,8 @@ def multiply(a, b):
 def divide_new(a, b):
   return a / b
 
-def get_median(a, b):
-  return (a+b)/2
+def test_func2(a, b):
+  return (a+b) / 2
 
 def test_func(a, b):
   return (a+b) / 2
