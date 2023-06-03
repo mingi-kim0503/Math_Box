2c0a20a3d94df5c2f5523bfe2d1a41469c29315e
MingiKim <danielkim.mingi@gmail.com>
2023-06-03 03:11:50 +0000

    sidj

--- calculator.py
+++ calculator.py
@@ -8,10 +8,10 @@ def multiply(a, b):
   return a * b
 
 def divide_new(a, b):
-  return a / b
+  return a / b * 2
 
 def test_func2(a, b):
-  return (a+b) / 2
+  return (a+b) / 3
 
 def test_func(a, b):
-  return (a+b) / 2
+  return (a+b) / 3
